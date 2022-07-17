93125ecdf8eed8c13ac8bb210aed69c311757522
Idania <idania.carolina@gmail.com>
2022-07-17 10:49:51 -0300

    varios

---
+++ PI_IdaniaCastellanos/BackEnd/src/main/java/com/portfolio/Idania/IdaniaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Idania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdaniaApplication.class, args);
+	}
+
+}
---
+++ PI_IdaniaCastellanos/BackEnd/src/test/java/com/portfolio/Idania/IdaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Idania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
